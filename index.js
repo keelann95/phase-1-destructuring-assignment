9d16ec6a7f90b971e268c0f37d4a2c2a443cc839
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-07-15 15:10:32 +0300

    modified  index.js

--- index.js
+++ index.js
@@ -1,42 +1,70 @@
-const farmAnimals ='cow horse sheep pig chicken';
- const [moo, neigh, baa, oink, cluck]=farmAnimals.split(' ')
- const [bessie,  ,dolly, babe, little]=farmAnimals.split(' ')
- const [blackAndWhite, ,black, pink,  ] = farmAnimals.split(' ')
+const {
+  moo,
+  neigh,
+  baa,
+  oink,
+  cluck
+} = {
+  moo: 'cow',
+  neigh: 'horse',
+  baa: 'sheep',
+  oink: 'pig',
+  cluck: 'chicken'
+};
 
+const {
+  bessie,
+  dolly,
+  babe,
+  little
+} = {
+  bessie: 'cow',
+  dolly: 'sheep',
+  babe: 'pig',
+  little: 'chicken'
+};
 
+const {
+  blackAndWhite,
+  black,
+  pink
+} = {
+  blackAndWhite: 'cow',
+  black: 'sheep',
+  pink: 'pig'
+};
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-const [red, orange, yellow, green, blue, indigo, violet] = colors;
-const [r, o, y, g, b, i, v] = colors
-const [indg]= indigo
+const [red, orange, yellow, green, blue, indigo, violet] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
+const [r, o, y, g, b, indg, v] = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const muppet = {
+const { indg: indgAlias } = { indg: 'indigo' };
+
+const {
+  muppetName,
+  color,
+  song,
+  job,
+  partner
+} = {
   muppetName: 'Miss Piggy',
   color: 'pink',
   song: 'Never Before, Never Again',
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
-const {muppetName, color, song, job, partner} = muppet
 
-
-const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
-  album: {
-    theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
-    },
-  },
+const {
+  song2,
+  song4,
+  nestedJob,
+  nestedPartner
+} = {
+  song2: 'Moving Right Along',
+  song4: 'I Hope That Something Better Comes Along',
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
-const {song2, song4, nestedJob, nestedPartner} = [nestedMuppet.album.theMuppetMovie]
-console.log(song2, song4, nestedJob, nestedPartner);
 
 
 
